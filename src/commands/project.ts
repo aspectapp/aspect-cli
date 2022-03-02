5dd00c17a477ab05fdc4f0a9700e1bcddd0931e1
Tshepo Mohlala <tshepohlala@gmail.com>
2022-03-01 23:51:21 -0800

    basic cli

--- src/commands/project.ts
+++ src/commands/project.ts
@@ -1,11 +1,14 @@
-const chalk = require('chalk');
-const axios = require('axios');
+// import {Chalk} from 'chalk';
+import {Axios} from 'axios';
 
-export async function fetchProjectComponents(
+async function fetchProjectComponents(
   projectId: string,
   source: string,
   options: any
 ) {
+  // const chalk = new Chalk();
+  const axios = new Axios();
+
   try {
     if (!projectId) {
       throw new Error('Project ID is required.');
@@ -13,29 +16,29 @@ export async function fetchProjectComponents(
     if (!options.key) {
       throw new Error('--key is required.');
     }
-
-    console.log(
-      chalk.blue.bold(`Fetching project components for project ${projectId}...`)
-    );
+    console.log(`Fetching project components for project ${projectId}...`);
 
     const urlBase = options.dev
       ? 'https://api.localhost/'
       : 'https://api.aspect.app/';
-    const response = await axios.post(urlBase + 'v2/fetch-project-components', {
+    const response = await axios.post(urlBase + 'v1/fetch-project-components', {
       projectId,
       key: options.key,
     });
-    console.log(chalk.greenBright(response));
+    console.log(response);
   } catch (error) {
-    console.error(chalk.red.bold(error));
+    console.error(error);
   }
 }
 
-export async function uploadProjectComponents(
+async function uploadProjectComponents(
   projectId: string,
   source: string,
   options: any
 ) {
+  // const chalk = new Chalk();
+  const axios = new Axios();
+
   try {
     if (!projectId) {
       throw new Error('Project ID is required.');
@@ -44,25 +47,24 @@ export async function uploadProjectComponents(
       throw new Error('--key is required.');
     }
 
-    console.log(
-      chalk.blue.bold(
-        `Uploading project components for project ${projectId}...`
-      )
-    );
+    console.log(`Uploading project components for project ${projectId}...`);
 
     // glob for all files in the source directory of type .jsx, .tsx, .css, .scss
+    // const files = await import('glob').then((glob) =>
     const urlBase = options.dev
       ? 'https://api.localhost/'
       : 'https://api.aspect.app/';
     const response = await axios.post(
-      urlBase + 'v2/upload-project-components',
+      urlBase + 'v1/upload-project-components',
       {
         projectId,
         key: options.key,
       }
     );
-    console.log(chalk.greenBright(response));
+    console.log(response);
   } catch (error) {
-    console.error(chalk.red.bold(error));
+    console.error(error);
   }
 }
+
+export {fetchProjectComponents, uploadProjectComponents};
